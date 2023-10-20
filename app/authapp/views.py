98b6742f5880d78f93a8b8e0aff06dc40a6e0240
Dmitry <hodas.work@gmail.com>
2023-10-20 21:46:50 +0300

    add views for authapp

--- app/authapp/views.py
+++ app/authapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib import messages
+from django.utils.safestring import mark_safe
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        ret = super().form_valid(form)
+        # message = _("Login success!<br>Hi, %(username)s") % {
+        #     "username": self.request.user.get_full_name()
+        #     if self.request.user.get_full_name()
+        #     else self.request.user.get_username()
+        # }
+        message = 'Login success!'
+        messages.add_message(self.request, messages.INFO, mark_safe(message))
+        return ret
+
+    def form_invalid(self, form):
+        for _unused, msg in form.error_messages.items():
+            messages.add_message(self.request,
+                                 messages.WARNING,
+                                 mark_safe(f"Something goes wrong:<br>{msg}"), )
+        return self.render_to_response(self.get_context_data(form=form))
