c6f443aeb6805079ed6ae71a52430798452b01ed
Dmitry <hodas.work@gmail.com>
2023-10-27 22:48:22 +0300

    make parts_page courses and use markdownify

--- app/mainapp/views.py
+++ app/mainapp/views.py
@@ -5,7 +5,9 @@ import mainapp.models as mainapp_models
 
 def index(request):
     context = {}
+    #  Курсы за исключением первого, первые три
     context['courses'] = mainapp_models.Course.objects.exclude(slug='first-course')[:3]
+    # Первый курс выбирается по слагу first-course
     context['first_course'] = get_object_or_404(mainapp_models.Course, slug='first-course')
     # return HttpResponse("Главная")
     return render(request, 'mainapp/index.html', context)
