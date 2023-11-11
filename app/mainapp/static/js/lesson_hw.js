72e5035a209e39b21642dcb74d74d350a25fe9ee
Dax <John-Xenos@yandex.ru>
2023-11-11 11:44:34 +0300

    отображение загруженных картинок в дз

--- app/mainapp/static/js/lesson_hw.js
+++ app/mainapp/static/js/lesson_hw.js
@@ -1,54 +1,66 @@
-document.querySelector("#account__les-hw-file-1f").addEventListener("change", function () {
-    if (this.files[0]) {
-      var fr = new FileReader();
+let accountLesHwFile1 = document.querySelector("#account__les-hw-file-1f");
+if (accountLesHwFile1) {
+    accountLesHwFile1.addEventListener("change", function () {
+        if (this.files[0]) {
+          var fr = new FileReader();
 
-      fr.addEventListener("load", function () {
-        document.querySelector("#account__les-hw-file-1l").style.backgroundImage = "url(" + fr.result + ")";
-        document.querySelector("#account__les-hw-file-1l").style.backgroundSize = "100%";
-      }, false);
+          fr.addEventListener("load", function () {
+            document.querySelector("#account__les-hw-file-1l").style.backgroundImage = "url(" + fr.result + ")";
+            document.querySelector("#account__les-hw-file-1l").style.backgroundSize = "100%";
+          }, false);
 
-      fr.readAsDataURL(this.files[0]);
-    }
-});
-
-
-document.querySelector("#account__les-hw-file-2f").addEventListener("change", function () {
-    if (this.files[0]) {
-      var fr = new FileReader();
-
-      fr.addEventListener("load", function () {
-        document.querySelector("#account__les-hw-file-2l").style.backgroundImage = "url(" + fr.result + ")";
-        document.querySelector("#account__les-hw-file-2l").style.backgroundSize = "100%";
-      }, false);
-
-      fr.readAsDataURL(this.files[0]);
-    }
-});
+          fr.readAsDataURL(this.files[0]);
+        }
+    });
+};
 
 
-document.querySelector("#account__les-hw-file-3f").addEventListener("change", function () {
-    if (this.files[0]) {
-      var fr = new FileReader();
+let accountLesHwFile2 = document.querySelector("#account__les-hw-file-2f");
+if (accountLesHwFile2) {
+    accountLesHwFile2.addEventListener("change", function () {
+        if (this.files[0]) {
+          var fr = new FileReader();
 
-      fr.addEventListener("load", function () {
-        document.querySelector("#account__les-hw-file-3l").style.backgroundImage = "url(" + fr.result + ")";
-        document.querySelector("#account__les-hw-file-3l").style.backgroundSize = "100%";
-      }, false);
+          fr.addEventListener("load", function () {
+            document.querySelector("#account__les-hw-file-2l").style.backgroundImage = "url(" + fr.result + ")";
+            document.querySelector("#account__les-hw-file-2l").style.backgroundSize = "100%";
+          }, false);
 
-      fr.readAsDataURL(this.files[0]);
-    }
-});
+          fr.readAsDataURL(this.files[0]);
+        }
+    });
+};
 
 
-document.querySelector("#account__les-hw-file-pdf").addEventListener("change", function () {
-    if (this.files[0]) {
-      var fr = new FileReader();
+let accountLesHwFile3 = document.querySelector("#account__les-hw-file-3f");
+if (accountLesHwFile3) {
+    accountLesHwFile3.addEventListener("change", function () {
+        if (this.files[0]) {
+          var fr = new FileReader();
 
-      fr.addEventListener("load", function () {
-        document.querySelector("#account__les-hw-file-pdfl").style.backgroundImage = "url(' {% static 'img/pdf.png' %}');";
-        document.querySelector("#account__les-hw-file-pdfl").style.backgroundSize = "87%";
-      }, false);
+          fr.addEventListener("load", function () {
+            document.querySelector("#account__les-hw-file-3l").style.backgroundImage = "url(" + fr.result + ")";
+            document.querySelector("#account__les-hw-file-3l").style.backgroundSize = "100%";
+          }, false);
 
-      fr.readAsDataURL(this.files[0]);
-    }
-});
+          fr.readAsDataURL(this.files[0]);
+        }
+    });
+};
+
+
+let accountLesHwFilePDF = document.querySelector("#account__les-hw-file-pdf");
+if (accountLesHwFilePDF) {
+    accountLesHwFilePDF.addEventListener("change", function () {
+        if (this.files[0]) {
+          var fr = new FileReader();
+
+          fr.addEventListener("load", function () {
+            document.querySelector("#account__les-hw-file-pdfl").style.backgroundImage = "url(' {% static 'img/pdf.png' %}');";
+            document.querySelector("#account__les-hw-file-pdfl").style.backgroundSize = "87%";
+          }, false);
+
+          fr.readAsDataURL(this.files[0]);
+        }
+    });
+};
